3db6180c0d2fe8c3ec7fa72ddfff590ea2b28285
blcmill <35299766+blcmill@users.noreply.github.com>
2018-01-10 08:55:31 -0500

    Updates hello.py, adds requirements

--- 01 hello.py
+++ 01 hello.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+#Blake Miller
+#10 Jan 2018
+#This assignment fulfills the requirements for the assignment "Hello Class."
+
 import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
+
+
 print('Hello, Class!')
+print('My name is Blake Miller.')
+print('My favorite game is The Wonderful 101.')
+print('I am concerned about the workload of this class.')
+print("I'm excited to see what kinds of games we will make in this class!")
+print("My stackoverflow number is 9198835, and a link to my profile is in my readme.")
+print("My github username is blcmill, and a link to my profile is in my readme.")
+print("Thank you for looking at my project!")
